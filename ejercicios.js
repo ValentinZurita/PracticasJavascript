3780ddd1a6ba74ed7aa2afc6da049ee3e10fb31f
valentinzurita <valentin-alejandro@hotmail.com>
2021-07-04 05:00:05 -0500

    Clase 41

--- ejercicios.js
+++ ejercicios.js
@@ -344,8 +344,46 @@ validarEmail("perro@gmail.com")
 
 //Clase 41 3 de Julio del 2021
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+ const arrayAlCuadrado = (arreglo = undefined) => {
+     if(arreglo === undefined) return console.warn(`No se ingreso un arreglo`)
+     if(!(arreglo instanceof Array)) return console.error(`El valor que ingresaste no es un arreglo`)
+     if(arreglo.length === 0) return console.error(`El arreglo esta vacio`)
+     for(let num of arreglo){
+         if(typeof num !== "number") return console.error(`En valor ${num} no es un numero`)
+     }
+     const nuevoArreglo = arreglo.map(e => e * e)
+     return console.info(`El arreglo al cuadrado: ${nuevoArreglo}`)
+ }
 
+ arrayAlCuadrado(true)
+ arrayAlCuadrado([1,2,3,4,5,5])
 
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+const arrayNumeroMasAltoYMasBajo = (arreglo = undefined) => {
+    if(arreglo === undefined) return console.warn(`No se ingreso un arreglo`)
+    if(!(arreglo instanceof Array)) return console.error(`El valor que ingresaste no es un arreglo`)
+    if(arreglo.length === 0) return console.error(`El arreglo esta vacio`)
+    for(let num of arreglo){
+        if(typeof num !== "number") return console.error(`En valor ${num} no es un numero`)
+    }
+    return console.info(`Arreglo original ${arreglo}\nValor mayor: ${Math.max(...arreglo)}\nValor menor: ${Math.min(...arreglo)}`)
+}
+
+arrayNumeroMasAltoYMasBajo(1)
+arrayNumeroMasAltoYMasBajo()
+arrayNumeroMasAltoYMasBajo("Hey como estas")
+arrayNumeroMasAltoYMasBajo([1,2,3,4,5,6])
+
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+const numerosParesImpares = (arreglo = undefined) =>{
+    return console.info({
+        pares: arreglo.filter(num => num % 2 === 0),
+        impares: arreglo.filter(num => num % 2 === 1)
+
+    })
+}
+
+numerosParesImpares([1,2,3,4,5,6,7,8,9])
+
+//Clase 42 4 de Julio del 2021
